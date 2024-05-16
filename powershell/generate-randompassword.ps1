6ddbf4d28e144ec67549db9e45d55f49615b752c
wolf-li <59823739+wolf-li@users.noreply.github.com>
2024-05-16 19:37:04 +0800

    Create generate-randompassword.ps1

---
+++ powershell/generate-randompassword.ps1
@@ -0,0 +1,17 @@
+function Generate-RandomPassword {
+    param (
+        [Parameter(Mandatory)]
+        [int] $length
+    )
+    $charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?'.ToCharArray()
+    $rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+    $bytes = New-Object byte[]($length)
+    $rng.GetBytes($bytes)
+    $result = New-Object char[]($length)
+    for ($i = 0 ; $i -lt $length ; $i++) {
+        $result[$i] = $charSet[$bytes[$i]%$charSet.Length]
+    }
+    return -join $result
+}
+
+Generate-RandomPassword 14
